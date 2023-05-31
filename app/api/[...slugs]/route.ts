6e69b8393af8128c034a9f0c98dc2f69f9cece7f
sunkeysun <sunkeysun@outlook.com>
2023-05-31 23:16:35 +0800

    feat: use fetch

--- app/api/[...slugs]/route.ts
+++ app/api/[...slugs]/route.ts
@@ -2,7 +2,7 @@ import https from 'https'
 import { NextRequest } from 'next/server';
 import { headers as NextHeders } from 'next/headers'
 
-const baseURL = 'https://api.openai.com:443/'
+const baseURL = 'https://api.openai.com/'
 
 export async function POST(request: NextRequest, { params }: { params: { slugs: string[] } }) {
   const { slugs } = params
@@ -12,42 +12,13 @@ export async function POST(request: NextRequest, { params }: { params: { slugs: 
   const urlObj = new URL(`${baseURL}${apiPath}`)
   const { authorization = '', ['content-type']: contentType = 'application/json' } = headers
 
-  const response = await new Promise<Response>((resolve, reject) => {
-    const request = https.request({
-      protocol: urlObj.protocol,
-      hostname: urlObj.hostname,
-      port: urlObj.port,
-      path: urlObj.pathname,
-      method: 'post',
-      headers: {
-        'content-type': contentType,
-        authorization,
-      },
-    }, (res) => {
-      const encoder = new TextEncoder()
-      const stream = new ReadableStream({
-        start(controller) {
-          res.on('data', (chunk) => {
-            controller.enqueue(encoder.encode(chunk))
-          })
-          res.on('end', () => {
-            controller.close()
-          })
-        },
-      })
-      const response = new Response(stream)
-      Object.entries(res.headers).forEach(([key, val]) => {
-        if (key && val && typeof val === 'string') {
-          response.headers.set(key, val)
-        }
-      })
-      resolve(response)
-    })
-    request.write(JSON.stringify(body))
-    request.end()
-    request.on('error', (err) => {
-      reject(err)
-    })
+  const response = await fetch(urlObj.toString(), {
+    method: 'post',
+    headers: {
+      'content-type': contentType,
+      authorization,
+    },
+    body,
   })
   return response
 }
